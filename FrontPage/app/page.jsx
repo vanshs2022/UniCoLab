8e5a3a0c203d2561916154063c08e886ce0b705b
Vansh Sharma <114303681+vanshs2022@users.noreply.github.com>
2025-01-21 19:28:46 +0530

    Update page.jsx
    Adding of Contact form to page (w.o. CSS)

--- FrontPage/app/page.jsx
+++ FrontPage/app/page.jsx
@@ -5,6 +5,8 @@ import Landing from "./Comp/Landing/page";
 import Mission from "./Comp/Mission/page";
 import  AboutUs from "./Comp/AboutUs/page";
 import Profiles from "./Comp/pROFILES/page";
+import Contact from "./Comp/Contact/page";
+
 const Page = () => {
   return (
     <div className="page">
@@ -12,8 +14,9 @@ const Page = () => {
       <AboutUs />
       <Profiles />
       <Mission />
+      <Contact />
     </div>
   );
 };
 
-export default Page; // Ensure this is a default export
+export default Page; 
